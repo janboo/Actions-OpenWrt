4c67e5d428c97b7207379bb94a5a715b66f9a923
Janboo <janboo@126.com>
2020-10-13 21:26:35 +0800

    bump to p3terx ver.9265b39

--- diy-part1.sh
+++ diy-part1.sh
@@ -6,9 +6,14 @@
 # Blog: https://p3terx.com
 #=================================================
 # Uncomment a feed source
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+#comment useless feed source
+sed -i 's/\(.*routing\)/#\0/' feeds.conf.default
+sed -i 's/\(.*telephony\)/#\0/' feeds.conf.default
+sed -i 's/\(.*freifunk\)/#\0/' feeds.conf.default
 
 # Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '1i src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '1i src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
