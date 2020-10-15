d982f04642841f73fc1ca39d1b450439e9842fba
Janboo <janboo@126.com>
2020-10-15 09:41:47 +0800

    fix naivproxy patch bugs

--- diy-part2.sh
+++ diy-part2.sh
@@ -7,3 +7,5 @@
 #=================================================
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.50.1/g' package/base-files/files/bin/config_generate
+# fix naivproxy 86.0.4240.75-1 build patch bug 2020/10/15
+sed -i 's/is_linux/is_linux \|\| is_chromeos/g' feeds/small/naiveproxy/patches/100-build-add-OpenWrt-staging-libraries.patch
