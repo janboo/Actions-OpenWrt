a19ca06425250fa94ad6849f2c34dab0c8634a55
janboo <janboo@126.com>
2021-07-14 18:16:21 +0800

    Add openwrt 21.02 version compile files

---
+++ diy-part3.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#=================================================
+# Description: DIY script
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=================================================
+
+#comment useless feed source
+sed -i 's/\(.*routing\)/#\0/' feeds.conf.default
+sed -i 's/\(.*telephony\)/#\0/' feeds.conf.default
+
+# Add a feed source
+sed -i '1i src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
